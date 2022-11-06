4f86a3c34ee3d1459a4ac19ae57f4e369f310fb4
waterfog9580 <waterfog9580@gmail.com>
2022-11-06 19:22:15 +0900

    :sparkles: 로그 추적기 V3 동시성 문제 해결

--- src/main/java/waterfogsw/springadvanced/config/LogTraceConfig.java
+++ src/main/java/waterfogsw/springadvanced/config/LogTraceConfig.java
@@ -3,14 +3,15 @@ package waterfogsw.springadvanced.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import waterfogsw.springadvanced.trace.logtrace.FieldLogTrace;
 import waterfogsw.springadvanced.trace.logtrace.LogTrace;
+import waterfogsw.springadvanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
   @Bean
   public LogTrace logTrace() {
-    return new FieldLogTrace();
+    return new ThreadLocalLogTrace();
   }
+
 }
